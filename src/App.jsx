c5aaff29345cfe5d9e0c7f6cd446ffd57b70f4a4
dearaujofelipe <mgdefelipe@outlook.com>
2024-05-28 20:22:18 -0300

    'router'

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from 'uuid';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { useState } from 'react';
 import Header from './components/Header';
 import FeedbackList from './components/FeedbackList';
 import FeedbackData from './data/FeedbackData';
 import FeedbackStats from './components/FeedbackStats';
 import FeedbackForm from './components/FeedbackForm';
+import AboutPage from './pages/AboutPage';
 
 export default function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -19,13 +21,27 @@ export default function App() {
   }
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <>
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
+                <FeedbackList
+                  feedback={feedback}
+                  handleDelete={deleteFeedback}
+                />
+              </>
+            }
+          ></Route>
+          <Route path="/about" element={<AboutPage />} />
+        </Routes>
       </div>
-    </>
+    </Router>
   );
 }
---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,18 @@
+import Card from '../components/shared/Card';
+
+function AboutPage() {
+  return (
+    <Card>
+      <div className="about">
+        <h1>about this project</h1>
+        <p>this is a react app to leave feedback for something</p>
+        <p>version 1.0.0</p>
+        <p>
+          <a href="">back to home</a>
+        </p>
+      </div>
+      ;
+    </Card>
+  );
+}
+export default AboutPage;
